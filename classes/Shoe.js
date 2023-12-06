575536529c36c61229fca5bad174a4c7ce789fa6
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-12-06 12:33:25 +0100

    feat: shoe.glb gets loaded in and has own class, threejs canvas gets displayed on

---
+++ classes/Shoe.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
+export class Shoe {
+  constructor(scene) {
+    this.scene = scene;
+    this.loadModel();
+  }
+
+  loadModel() {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderConfig({ type: 'js' });
+    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
+
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.setDRACOLoader(dracoLoader); // Set DRACOLoader instance
+    
+    gltfLoader.load("/models/shoe.glb", (gltf) => {
+      gltf.scene.position.set(0, 0, 0);
+      gltf.scene.rotation.y = 1;
+      gltf.scene.scale.set(6, 6, 6);
+
+      let shoe = gltf.scene.children[0];
+
+      shoe.traverse((child) => {
+        if (child.isMesh) {
+          console.log(child.name);
+          this.setMeshProperties(child);
+        }
+      });
+      this.scene.add(gltf.scene);
+    });
+  }
+
+  setMeshProperties(mesh) {
+    switch (mesh.name) {
+      case "inside":
+      case "outside_1":
+      case "outside_2":
+      case "outside_3":
+      case "laces":
+      case "sole_bottom":
+      case "sole_top":
+        mesh.material.color.set("#ffffff");
+        break;
+    }
+    // Set up casting shadows for meshes
+    mesh.castShadow = true;
+  }
+  
+}
--- main.js
+++ main.js
@@ -1,1 +1,55 @@
-import './style.css'
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Shoe } from './classes/Shoe.js';
+
+const draco = new DRACOLoader();
+draco.setDecoderConfig({ type: 'js' });
+draco.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.6/');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// Get the canvasContainer div
+const canvasContainer = document.getElementById('canvasContainer');
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
+canvasContainer.appendChild(renderer.domElement);
+
+// controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Resize function.
+resize();
+window.addEventListener("resize", resize);
+function resize() {
+    renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
+    camera.aspect = canvasContainer.clientWidth / canvasContainer.clientHeight;
+    camera.updateProjectionMatrix();
+}
+
+//add GLTF model
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(draco);
+
+// Shoe
+// Set up the GLTF model
+const shoe = new Shoe(scene);
+
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.9);
+scene.add(ambientLight);
+
+
+camera.position.z = 3;
+camera.position.y = 1;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+animate();
